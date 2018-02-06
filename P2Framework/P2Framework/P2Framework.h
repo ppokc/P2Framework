0ccc7a21f5683669d708ad26d8bbc95d510ee888
Phillips Pham <ppham@PhilPhamMacBookPro.local>
2018-02-06 15:33:55 -0600

    Done

---
+++ P2Framework/P2Framework/P2Framework.h
@@ -0,0 +1,19 @@
+//
+//  P2Framework.h
+//  P2Framework
+//
+//  Created by Phillips Pham on 2/6/18.
+//  Copyright © 2018 P2QSOFTWARE LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for P2Framework.
+FOUNDATION_EXPORT double P2FrameworkVersionNumber;
+
+//! Project version string for P2Framework.
+FOUNDATION_EXPORT const unsigned char P2FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <P2Framework/PublicHeader.h>
+
+
---
+++ P2Framework/P2Framework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  P2Framework
+//
+//  Created by Phillips Pham on 2/6/18.
+//  Copyright © 2018 P2QSOFTWARE LLC. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    
+    public static func getVersion() -> String {
+        return "1.0.0"
+    }
+}
